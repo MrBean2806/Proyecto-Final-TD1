087ec0d489eef00700e047c13529050995f38b0a
agus <agusbean14@gmail.com>
2021-01-29 13:35:51 -0300

    Los ciclos son iguales a RGB Elimine start Reestructuracion del case

--- temporizador.v
+++ temporizador.v
@@ -1,64 +1,71 @@
-module temporizador (
+	module temporizador (
     input clk,
     input enter,
     input [4:0] R, G, B,
     output flag_R, flag_G, flag_B
 );
-parameter ciclos_max = 500_000; //ciclos
-reg [4:0] ciclos_R, ciclos_G, ciclos_B;
-reg [31:0] contador = 0;
-reg [2:0] RBG_count = 0;       //indica que color se esta cargando
+parameter ciclos_max = 15; //ciclos
+parameter ciclo_unitario = 5;
+reg [4:0] ciclos_R, ciclos_G, ciclos_B;	//hay q agrandarlo en funcion de ciclo_unitario pq la multiplicacion puede dar valores grandes
+reg [3:0] contador = 0;				//cuenta solo hasta 15
+reg [1:0] RGB_count = 0;       //indica que color se esta cargando
+//reg start = 0;
 
 always @(posedge clk) begin
-    if(B != 5'd16 )  begin       //Si ya pasaron los estados de lectura
-        ciclos_R <= ciclos_max*R/15;
-        ciclos_G <= ciclos_max*G/15;
-        ciclos_B <= ciclos_max*B/15;
-    end
+    if( B != 5'd16 /* && start*/)  begin       //Si ya pasaron los estados de lectura
+        ciclos_R <= R;
+        ciclos_G <= G;
+        ciclos_B <= B;
+	 
+	 end
 end
 
-always @(posedge clk) begin
-    if(enter)
-        start <= 1;
-    else 
-        start <= start;
+/*always @(posedge clk) begin
+    //no se si hace falta start
+    begin
+		 if(enter)
+			  start <= 1;
+		 else 
+			  start <= start;
+		end
 end
+	*/	
 
 always @(posedge clk) begin
-    case (RGB_count)
-        3'b001: begin
-						 if(start)   contador <= contador + 1;
-						 else contador <= contador; //=0
-						 if(contador > ciclos_R) begin
-							 RBG_count <= 3'b010;
-							 contador <= 0;
-							end
-					 end
-        3'b010: if(contador > ciclos_G) begin
-                    RBG_count <= 3'b100;
+	 case (RGB_count)
+        //agregarle shift aritmetico
+        2'b00: begin
+                    if(enter)   RGB_count <= 2'b01; 
+                    else        RGB_count <= RGB_count; //hace falta?
+                end
+        2'b01: begin
+                    if(contador > ciclos_R) begin
+                        RGB_count <= 2'b10;    //paso al sig color
+                        contador <= 0;          
+                    end
+                    else contador <= contador + 1;
+                end
+        2'b10: if(contador > ciclos_G) begin
+                    RGB_count <= 2'b11;
                     contador <= 0;
                   end
                 else contador <= contador + 1;
-        3'b100: if(contador > ciclos_B) begin
-                    start <= 0;
-                    RBG_count <= 3'b001;
+        2'b11: if(contador > ciclos_B) begin
+                    //start <= 0;
+                    RGB_count <= 2'b00;
                     contador <= 0;
                 end
                 else contador <= contador + 1;
-        default:  begin
-                    start <= 0;
-                    RBG_count <= 3'b001;
-                    contador <= 0;
-                  end  
+        //no hace falta el default pq estan incluidos todos los valores de RGB_count
     endcase
 end
 
 
 //Se activa la bandera cuando paso el tiempo de prendido de cada motor
 //Duran un ciclo de clk en 1, despues los ifs de arriba resetean los contadores
-assign flag_R = (contador_R >= ciclos_R);
-assign flag_G = (contador_G >= ciclos_G);
-assign flag_B = (contador_B >= ciclos_B);
+assign flag_R = (contador >= ciclos_R);
+assign flag_G = (contador >= ciclos_G);
+assign flag_B = (contador >= ciclos_B);
 
 
 endmodule
