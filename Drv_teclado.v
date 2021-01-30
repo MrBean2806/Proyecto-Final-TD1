9b8adec497e747662238b7dcca9ddc763c463d32
agus <agusbean14@gmail.com>
2021-01-30 12:08:07 -0300

    parametrizado de fila y col

--- Drv_teclado.v
+++ Drv_teclado.v
@@ -10,6 +10,12 @@ module Drv_teclado(
 reg [4:0] aux = 5'b10000;
 reg [3:0] counter = 0;
 
+//Cuando el bit de alguna posicion está en 1, significa q esa tecla esta presionada
+parameter fila_1 = 4'b0001, col_1 = 4'b0001,
+			 fila_2 = 4'b0010, col_2 = 4'b0010,
+			 fila_3 = 4'b0100, col_3 = 4'b0100,
+			 fila_4 = 4'b1000, col_4 = 4'b1000;
+
 always @(posedge clk) begin
 	if(fila == 4'd0) begin
 		digito <= digito;
@@ -22,7 +28,6 @@ always @(posedge clk) begin
 		
 		digito <= aux;
 	end
-	//shift aritmetico
 	begin
 		col <= col <<< 1;
 		if(col == 4'b1000)
@@ -33,39 +38,39 @@ end
 
 always @(fila, col, digito, aux) begin
 	case(col)
-		4'b0001: begin
+		col_1: begin
 					case(fila)
-						4'b0001: aux <= 5'd1;
-						4'b0010: aux <= 5'd4;
-						4'b0100: aux <= 5'd7;
-						4'b1000: aux <= 5'hF;
+						fila_1: aux <= 5'd1;
+						fila_2: aux <= 5'd4;
+						fila_3: aux <= 5'd7;
+						fila_4: aux <= 5'hF;
 						default: aux <= 5'd16;
 					endcase
 				 end
-		4'b0010: begin
+		col_2: begin
 					case(fila)
-						4'b0001: aux <= 5'd2;
-						4'b0010: aux <= 5'd5;
-						4'b0100: aux <= 5'd8;
-						4'b1000: aux <= 5'd0;
+						fila_1: aux <= 5'd2;
+						fila_2: aux <= 5'd5;
+						fila_3: aux <= 5'd8;
+						fila_4: aux <= 5'd0;
 						default: aux <= 5'd16;
 					endcase
 				 end
-		4'b0100: begin
+		col_3: begin
 					case(fila)
-						4'b0001: aux <= 5'd3;
-						4'b0010: aux <= 5'd6;
-						4'b0100: aux <= 5'd9;
-						4'b1000: aux <= 5'hE;
+						fila_1: aux <= 5'd3;
+						fila_2: aux <= 5'd6;
+						fila_3: aux <= 5'd9;
+						fila_4: aux <= 5'hE;
 						default: aux <= 5'd16;
 					endcase
 				 end
-		4'b1000: begin
+		col_4: begin
 					case(fila)
-						4'b0001: aux <= 5'hA;
-						4'b0010: aux <= 5'hB;
-						4'b0100: aux <= 5'hC;
-						4'b1000: aux <= 5'hD;
+						fila_1: aux <= 5'hA;
+						fila_2: aux <= 5'hB;
+						fila_3: aux <= 5'hC;
+						fila_4: aux <= 5'hD;
 						default: aux <= 5'd16;
 					endcase
 				 end
