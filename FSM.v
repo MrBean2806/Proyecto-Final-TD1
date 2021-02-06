cd6cc855fc33d51994c606d997bc3a737e11b40f
agus <agusbean14@gmail.com>
2021-02-06 19:09:44 -0300

    Unifico estados de lectura Arreglo latch de estado_pos por fin

--- FSM.v
+++ FSM.v
@@ -1,40 +1,35 @@
 module FSM(
     input clk,
     input reset,
-    //input [4:0] digito,
+    input RGB_full,
     input [2:0] flags,
-    input cambio,
+    //input cambio,
     input enter,
     output reg [2:0] Motores
 );
     parameter r = 2'd2, g = 2'd1, b = 2'd0;
-    parameter lectura_R = 3'b0, lectura_Y = 3'b001, lectura_B = 3'b010,
-              carga_R = 3'b011, carga_Y = 3'b100, carga_B = 3'b101, espera = 3'b110;
+    parameter lectura = 3'b0, espera = 3'b001,
+              carga_R = 3'b011, carga_Y = 3'b100, carga_B = 3'b101;
     
-    reg [2:0] estado, estado_pos;
+    reg [2:0] estado, estado_pos = 0;
 
     always @ (posedge clk or negedge reset) begin
         if( !reset )
-            estado <= lectura_R;
+            estado <= lectura;
         else estado <= estado_pos;
     end
 
     always @ (*)   begin
-        estado_pos = estado_pos;
         case(estado)
-            lectura_R:  if(cambio) estado_pos = lectura_Y;
-                        else estado_pos = estado_pos;
-            lectura_Y:  if(!cambio) estado_pos = lectura_B;
-                        else estado_pos = estado_pos;
-            lectura_B:  if(cambio) estado_pos = espera;
-                        else estado_pos = estado_pos;
-            espera:     if(!cambio) estado_pos = lectura_R;
+            lectura:    if(RGB_full) estado_pos = espera;
+                        else estado_pos = estado;
+            espera:     if(!RGB_full) estado_pos = lectura;
                         else if(enter)  estado_pos = carga_R;
-                        else estado_pos = estado_pos;
-            carga_R:    if( flags[r] ) estado_pos = carga_Y;    else estado_pos = estado_pos;
-            carga_Y:    if( flags[g] ) estado_pos = carga_B;    else estado_pos = estado_pos;
-            carga_B:    if( flags[b] ) estado_pos = lectura_R;  else estado_pos = estado_pos;
-			default: 	estado_pos = estado_pos;
+                        else estado_pos = estado;
+            carga_R:    if( flags[r] ) estado_pos = carga_Y;    else estado_pos = estado;
+            carga_Y:    if( flags[g] ) estado_pos = carga_B;    else estado_pos = estado;
+            carga_B:    if( flags[b] ) estado_pos = lectura;  else estado_pos = estado;
+			default: 	estado_pos = lectura;
         endcase
     end
 
@@ -42,9 +37,7 @@ module FSM(
         case (estado)
             // 5'd16 -> display en blanco
             // 5'd17 -> guion
-            lectura_R: Motores = 3'b000;
-            lectura_Y: Motores = 3'b000;
-            lectura_B: Motores = 3'b000;
+            lectura: Motores = 3'b000;
             espera:    Motores = 3'b000;
             carga_R:   Motores = 3'b100;
             carga_Y:   Motores = 3'b010;
