ac3bf88da2ac8eafb0b6f2eb8dba4a512302abc2
agus <agusbean14@gmail.com>
2021-02-05 21:05:32 -0300

    Elimino latch inferido

--- FSM.v
+++ FSM.v
@@ -2,22 +2,25 @@ module FSM(
     input clk,
     input reset,
     //input [4:0] digito,
-    input flag_R, flag_B, flag_G,
+    input [2:0] flags,
     input cambio,
     input enter,
     output reg [2:0] Motores
 );
+    parameter r = 2'd2, g = 2'd1, b = 2'd0;
     parameter lectura_R = 3'b0, lectura_Y = 3'b001, lectura_B = 3'b010,
               carga_R = 3'b011, carga_Y = 3'b100, carga_B = 3'b101, espera = 3'b110;
+    
     reg [2:0] estado, estado_pos;
 
-    always @ (posedge clk or negedge rst) begin
+    always @ (posedge clk or negedge reset) begin
         if( !reset )
             estado <= lectura_R;
         else estado <= estado_pos;
     end
 
     always @ (*)   begin
+        estado_pos = estado_pos;
         case(estado)
             lectura_R:  if(cambio) estado_pos = lectura_Y;
                         else estado_pos = estado_pos;
@@ -28,9 +31,10 @@ module FSM(
             espera:     if(!cambio) estado_pos = lectura_R;
                         else if(enter)  estado_pos = carga_R;
                         else estado_pos = estado_pos;
-            carga_R:    if(flag_R) estado_pos = carga_Y;    else estado_pos = estado_pos;
-            carga_Y:    if(flag_G) estado_pos = carga_B;    else estado_pos = estado_pos;
-            carga_B:    if(flag_B) estado_pos = lectura_R;  else estado_pos = estado_pos;
+            carga_R:    if( flags[r] ) estado_pos = carga_Y;    else estado_pos = estado_pos;
+            carga_Y:    if( flags[g] ) estado_pos = carga_B;    else estado_pos = estado_pos;
+            carga_B:    if( flags[b] ) estado_pos = lectura_R;  else estado_pos = estado_pos;
+			default: 	estado_pos = estado_pos;
         endcase
     end
 
